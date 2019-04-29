4fdeccfe575f7f7ea5a2f6ffa07d59c24deb6a61
dccfmatos <d.catarina.c.f@gmail.com>
2019-04-29 21:52:12 +0100

    Initial commit

---
+++ src/barkingDog.java
@@ -0,0 +1,19 @@
+public class barkingDog {
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+
+        if (!barking){
+            return false;
+        } else if (hourOfDay < 0 || hourOfDay > 23){
+             return false;
+            }else if (hourOfDay < 8 || hourOfDay > 22){
+                          return true;
+                }
+
+        return false;
+            }
+
+        }
+
+
+
